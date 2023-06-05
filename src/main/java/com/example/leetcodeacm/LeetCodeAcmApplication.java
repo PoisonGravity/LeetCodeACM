7cee5049a7e2a4a07906042fec8bb3ae267fbc75
PoisonGravity <107403926+PoisonGravity@users.noreply.github.com>
2023-06-05 11:30:26 +0100

    LeetCodeACM initialization

---
+++ src/main/java/com/example/leetcodeacm/LeetCodeAcmApplication.java
@@ -0,0 +1,13 @@
+package com.example.leetcodeacm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeAcmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodeAcmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/leetcodeacm/LeetCodeAcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leetcodeacm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeAcmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
